6c3b95d798fcb914478099d780faabb4fa610e2f
Daniele Guarascio <daniele.guarascio@prima.it>
2022-01-21 15:35:51 +0100

    make gauge macro more usable accepting an expression

--- src/macros/gauge.rs
+++ src/macros/gauge.rs
@@ -1,22 +1,22 @@
 /// Report an arbitrary value as a gauge
 #[macro_export]
 macro_rules! gauge {
-    ($stat:literal, $val:literal) => {
+    ($stat:literal, $val:expr) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().gauge($stat, $val, vec![]);
         }
     };
-    ($stat:path, $val:literal) => {
+    ($stat:path, $val:expr) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().gauge($stat.as_ref(), $val, vec![]);
         }
     };
-    ($stat:literal, $val:literal; $( $key:expr => $value:expr ), *) => {
+    ($stat:literal, $val:expr; $( $key:expr => $value:expr ), *) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().gauge($stat, $val, std::vec![$(std::format!("{}:{}", $key, $value)), *]);
         }
     };
-    ($stat:path, $val:literal; $( $key:expr => $value:expr ), *) => {
+    ($stat:path, $val:expr; $( $key:expr => $value:expr ), *) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().gauge($stat.as_ref(), $val, std::vec![$(std::format!("{}:{}", $key, $value)), *]);
         }
