4aa1d90299ee943b8005c1791f4d940ecf7d0029
fabuloso <dalfovo.alessandro@gmail.com>
2022-02-08 16:39:29 +0100

    Accept any expression as metadata value
    Also introduce an integration test passing by macro

--- src/macros/event.rs
+++ src/macros/event.rs
@@ -1,27 +1,17 @@
 /// Send a custom event as a title and a body
 #[macro_export]
 macro_rules! event {
-    ($stat:literal, $text:literal) => {
-        if $crate::Datadog::global().is_reporting_enabled() {
-            $crate::Datadog::global().event($stat, $text, vec![]);
-        }
-    };
     ($stat:path, $text:literal) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().event($stat.as_ref(), $text, vec![]);
         }
     };
-    ($stat:literal, $text:ident) => {
+    ($stat:literal, $text:expr) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().event($stat, $text, vec![]);
         }
     };
-    ($stat:literal, $text:literal; $( $key:expr => $value:expr ), *) => {
-        if $crate::Datadog::global().is_reporting_enabled() {
-            $crate::Datadog::global().event($stat, $text, std::vec![$(std::format!("{}:{}", $key, $value)), *]);
-        }
-    };
-    ($stat:literal, $text:ident; $( $key:expr => $value:expr ), *) => {
+    ($stat:literal, $text:expr; $( $key:expr => $value:expr ), *) => {
         if $crate::Datadog::global().is_reporting_enabled() {
             $crate::Datadog::global().event($stat, $text, std::vec![$(std::format!("{}:{}", $key, $value)), *]);
         }
---
+++ tests/integration.rs
@@ -0,0 +1,54 @@
+use once_cell::sync::OnceCell;
+use prima_datadog::{configuration::PrimaConfiguration, Datadog};
+use serial_test::serial;
+use std::net::UdpSocket;
+
+static SOCKET: OnceCell<UdpSocket> = OnceCell::new();
+
+#[test]
+#[serial]
+fn test_event_notification_with_variable_as_description() {
+    let socket = init_test_datadog();
+
+    let variable = "variabile";
+    prima_datadog::event!("test", variable);
+
+    let event = read_as_string(socket);
+
+    assert!(event.contains(variable))
+}
+
+#[test]
+#[serial]
+fn test_event_notification_with_literal_as_description() {
+    let socket = init_test_datadog();
+
+    prima_datadog::event!("test", "variable");
+
+    let event = read_as_string(socket);
+    assert!(event.contains("variable"))
+}
+
+fn read_as_string(socket: &UdpSocket) -> String {
+    let mut buf = [0; 50];
+    let (length, _) = socket
+        .recv_from(&mut buf)
+        .expect("Could not read from socket");
+    let buf = &buf[..length];
+    String::from_utf8(buf.to_vec()).unwrap()
+}
+
+fn init_test_datadog() -> &'static UdpSocket {
+    let socket =
+        SOCKET.get_or_init(|| UdpSocket::bind("127.0.0.1:0").expect("couldn't open udp socket"));
+    let address_to = format!("127.0.0.1:{}", socket.local_addr().unwrap().port());
+
+    let configuration = PrimaConfiguration::new(
+        &address_to,
+        "0.0.0.0:0",
+        "prova_datadog",
+        "production".parse().unwrap(),
+    );
+    let _ = Datadog::init(configuration);
+    socket
+}
