4341b496d0cafbc38839e36ddeb2e0e524d85ea7
Cristiano Piemontese <cristiano.piemontese@prima.it>
2024-11-25 15:29:15 +0100

    add macros with options

--- src/macros/event_with_options.rs
+++ src/macros/event_with_options.rs
@@ -5,19 +5,37 @@ macro_rules! event_with_options {
     ($stat:path, $text:expr) => {
         $crate::Datadog::event_with_options($stat.as_ref(), $text, $crate::EMPTY_TAGS, None);
     };
+    ($stat:path, $text:expr, $options:expr) => {
+        $crate::Datadog::event_with_options($stat.as_ref(), $text, $crate::EMPTY_TAGS, Some($options));
+    };
     ($stat:expr, $text:expr) => {
         $crate::Datadog::event_with_options($stat, $text, $crate::EMPTY_TAGS, None);
     };
-    ($stat:expr, $text:expr; $( $key:literal => $value:literal ), *) => {
-        $crate::Datadog::event_with_options($stat, $text, &[$(::core::concat!($key, ":", $value)), *], None);
+    ($stat:expr, $text:expr, $options:expr) => {
+        $crate::Datadog::event_with_options($stat, $text, $crate::EMPTY_TAGS, Some($options));
     };
     ($stat:path, $text:expr; $( $key:literal => $value:literal ), *) => {
         $crate::Datadog::event_with_options($stat.as_ref(), $text, &[$(::core::concat!($key, ":", $value)), *], None);
     };
-    ($stat:expr, $text:expr; $( $key:expr => $value:expr ), *) => {
-        $crate::Datadog::event_with_options($stat, $text, &[$(::std::format!("{}:{}", $key, $value).as_str()), *], None);
+    ($stat:path, $text:expr, $options:expr; $( $key:literal => $value:literal ), *) => {
+        $crate::Datadog::event_with_options($stat.as_ref(), $text, &[$(::core::concat!($key, ":", $value)), *], Some(options));
+    };
+    ($stat:expr, $text:expr; $( $key:literal => $value:literal ), *) => {
+        $crate::Datadog::event_with_options($stat, $text, &[$(::core::concat!($key, ":", $value)), *], None);
+    };
+    ($stat:expr, $text:expr, $options:expr; $( $key:literal => $value:literal ), *) => {
+        $crate::Datadog::event_with_options($stat, $text, &[$(::core::concat!($key, ":", $value)), *], Some(options));
     };
     ($stat:path, $text:expr; $( $key:expr => $value:expr ), *) => {
         $crate::Datadog::event_with_options($stat.as_ref(), $text, &[$(::std::format!("{}:{}", $key, $value).as_str()), *], None);
+    };
+    ($stat:path, $text:expr, $options:expr; $( $key:expr => $value:expr ), *) => {
+        $crate::Datadog::event_with_options($stat.as_ref(), $text, &[$(::std::format!("{}:{}", $key, $value).as_str()), *], Some($options));
+    };
+    ($stat:expr, $text:expr; $( $key:expr => $value:expr ), *) => {
+        $crate::Datadog::event_with_options($stat, $text, &[$(::std::format!("{}:{}", $key, $value).as_str()), *], None);
+    };
+    ($stat:expr, $text:expr, $options:expr; $( $key:expr => $value:expr ), *) => {
+        $crate::Datadog::event_with_options($stat, $text, &[$(::std::format!("{}:{}", $key, $value).as_str()), *], Some($options));
     };
 }
